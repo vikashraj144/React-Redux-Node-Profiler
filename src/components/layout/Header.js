a837dd513313c16eda1c162719d592f15dda9650
vikash <vikashraj144@gmail.com>
2019-03-03 11:47:26 +0530

    header added

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,21 +7,18 @@ import {
     NavbarBrand,
     Nav,
     NavItem,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem } from 'reactstrap';
+} from 'reactstrap';
 const Header = () => {
 
     const links = [
-        {name: 'Practice', url: '/about'},
+        {name: 'About', url: '/about'},
         {name: '404 Error', url: '/404Error'},
 
     ];
 
     let linksComponents = links.map((link, index) => {
         return (
-            <li key={index} className={'nav'}>
+            <li key={index} className={'nav-item'}>
                 <NavLink className={'navLink'} activeClassName={'activeNavLink'}
                          to={link.url} exact>{link.name}</NavLink>
             </li>
@@ -29,27 +26,19 @@ const Header = () => {
     });
 
     return (
-        // <div className={'leftNavContainer'}>
         <div className='span4'>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
-          <NavbarToggler  />
-          <Collapse navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink className={'navLink'} activeClassName={'activeNavLink'} to='/' exact>Home</NavLink>                
-              </NavItem>
-                {linksComponents}
-
-            </Nav>
-          </Collapse>
-        </Navbar>
-            {/* <ul>
-                <li className='nav'>
-                    <NavLink className={'navLink'} activeClassName={'activeNavLink'} to='/' exact>Home</NavLink>
-                </li>
-                {linksComponents}
-            </ul> */}
+            <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">reactstrap</NavbarBrand>
+            <NavbarToggler  />
+            <Collapse navbar>
+                <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <NavLink className={'navLink'} activeClassName={'activeNavLink'} to='/' exact>Home</NavLink>                
+                </NavItem>
+                    {linksComponents}
+                </Nav>
+            </Collapse>
+            </Navbar>
         </div>
     );
 };
