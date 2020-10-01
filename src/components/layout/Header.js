0cecd484b83e6690bbf1c6f293d8d7585f37d00c
vikash <vikashraj144@gmail.com>
2020-10-01 21:18:46 +0530

    code clean

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,48 +1,51 @@
 import React from "react";
-import {NavLink} from 'react-router-dom';
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-} from 'reactstrap';
+import { NavLink } from "react-router-dom";
+import { Collapse, Nav, Navbar, NavbarToggler } from "reactstrap";
 const Header = () => {
+  const links = [
+    { name: "About", url: "/about" },
+    { name: "Experience", url: "/experience" },
+    { name: "404 Error", url: "/404Error" },
+  ];
 
-    const links = [
-        {name: 'About', url: '/about'},
-        {name: 'Experience', url: '/experience'},
-        {name: '404 Error', url: '/404Error'},
-
-    ];
-
-    let linksComponents = links.map((link, index) => {
-        return (
-            <li key={index} className={'nav-item'}>
-                <NavLink className={'navLink'} activeClassName={'activeNavLink'}
-                         to={link.url} exact>{link.name}</NavLink>
-            </li>
-        );
-    });
-
+  let linksComponents = links.map((link, index) => {
     return (
-        <div className='span4'>
-            <Navbar color="light" light expand="md">
-                    <NavLink className={'navLink'} activeClassName={'activeNavLink'} to='/' exact>Profiler</NavLink>                
-            <NavbarToggler  />
-            <Collapse navbar>
-                <Nav className="ml-auto" navbar>
-                {/* <NavItem>
+      <li key={index} className={"nav-item"}>
+        <NavLink
+          className={"navLink"}
+          activeClassName={"activeNavLink"}
+          to={link.url}
+          exact
+        >
+          {link.name}
+        </NavLink>
+      </li>
+    );
+  });
+
+  return (
+    <div className="span4">
+      <Navbar color="light" light expand="md">
+        <NavLink
+          className={"navLink"}
+          activeClassName={"activeNavLink"}
+          to="/"
+          exact
+        >
+          Profiler
+        </NavLink>
+        <NavbarToggler />
+        <Collapse navbar>
+          <Nav className="ml-auto" navbar>
+            {/* <NavItem>
                     <NavLink className={'navLink'} activeClassName={'activeNavLink'} to='/' exact>Home</NavLink>                
                 </NavItem> */}
-                    {linksComponents}
-                </Nav>
-            </Collapse>
-            </Navbar>
-        </div>
-    );
+            {linksComponents}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 };
-
 
 export default Header;
