5acd4e13259a92557c09ca0fe8f0525af5a3e393
vikash <vikashraj144@gmail.com>
2020-10-01 21:13:40 +0530

    image removed

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const PageNotFound = () => {
+  const style404 = {
+    fontSize: 72,
+    fontWeight: "bold",
+  };
 
-    const style404 = {
-        fontSize: 72,
-        fontWeight: 'bold'
-    };
-
-    return (
-        <div style={{textAlign:'center'}}>
-            <h1>Page Not Found</h1>
-            <div style={style404}>404</div>
-            <img src={'/'} />
-        </div>
-    );
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Page Not Found</h1>
+      <div style={style404}>404</div>
+    </div>
+  );
 };
 
 export default PageNotFound;
