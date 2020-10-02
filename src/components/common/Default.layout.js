d6c44327854ab8febde93a7577fe68194a824ee5
vikash <vikashraj144@gmail.com>
2020-10-02 07:30:20 +0530

    default layout

---
+++ src/components/common/Default.layout.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import FontStyles from "../../styles/font-styles";
+import GlobalStyles from "../../styles/global-styles";
+import Footer from "../layout/Footer";
+import Header from "../layout/Header";
+import Container from "../styled/Container.styled";
+
+const Main = styled.main`
+  flex: 1;
+`;
+
+const DefaultLayout = ({ children }) => {
+  return (
+    <Container>
+      <FontStyles />
+      <GlobalStyles />
+      <Header />
+      <Main>{children}</Main>
+      <Footer />
+    </Container>
+  );
+};
+
+DefaultLayout.propTypes = {};
+
+export default DefaultLayout;
