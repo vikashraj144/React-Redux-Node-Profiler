40bc222f7e3fbb77408a9d7a2778f66067123df5
vikash <vikashraj144@gmail.com>
2020-10-02 07:27:14 +0530

    move to pages

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/common/Default.layout";
+const Home = (props) => {
+  return (
+    <Layout>
+      <h1 className="display-3">Vikash Kumar Verma</h1>
+      <p className="lead">Front-end Developer</p>
+    </Layout>
+  );
+};
+
+Home.propTypes = {};
+
+export default Home;
