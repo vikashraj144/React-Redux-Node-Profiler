74bf9affd06f68e99b16ef1220d51e20f6db5b6a
vikash <vikashraj144@gmail.com>
2020-10-03 17:57:01 +0530

    home component up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
-import Layout from "../components/common/Default.layout";
+import Carousal from "../components/common/slider/Carousal.component";
+import Layout from "../components/layout/Default.layout";
 const Home = (props) => {
   return (
     <Layout>
+      <Carousal />
       <h1 className="display-3">Vikash Kumar Verma</h1>
       <p className="lead">Front-end Developer</p>
     </Layout>
