f99faa0032c5c7f3cab1098a55b27d18c1e8f0b2
vikash <vikashraj144@gmail.com>
2020-10-04 21:51:08 +0530

    home component added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
+import AboutMe from "../components/common/AboutMe.component";
+import Award from "../components/common/Award.component";
+import Resume from "../components/common/Resume.component";
 import Carousal from "../components/common/slider/Carousal.component";
 import Layout from "../components/layout/Default.layout";
 const Home = (props) => {
   return (
     <Layout>
       <Carousal />
+      <AboutMe />
+      <Resume />
+      <Award />
       <h1 className="display-3">Vikash Kumar Verma</h1>
       <p className="lead">Front-end Developer</p>
     </Layout>
