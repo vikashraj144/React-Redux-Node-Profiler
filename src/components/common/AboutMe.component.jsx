198fbb62c70ad747ea2cffded10d318b7b32fe2e
vikash <vikashraj144@gmail.com>
2020-12-25 10:56:08 +0530

    about me

--- src/components/common/AboutMe.component.jsx
+++ src/components/common/AboutMe.component.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import ComponentLayout from "../layout/Component.layout";
 
 const ListItem = styled.div`
-  background-color: red;
   display: flex;
   width: 20%;
   flex-wrap: wrap;
@@ -16,43 +15,35 @@ const Container = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   align-items: stretch;
-
-  background-color: DodgerBlue;
+  // background-color: DodgerBlue;
 `;
 
 const AboutMe = () => {
   return (
     <ComponentLayout>
       <h2 style={{ textAlign: "center" }}>About Me</h2>
+      <h2 style={{ textAlign: "center" }}> | </h2>
       <Container>
         <ListItem style={{ flexGrow: 1 }}>
-          <img src="./profile_pic.jpeg" height={450} width={500} />
-          {/* <h3>Who am i ?</h3>
-          <p>
-            Is ut graece delectus repudiare, in habeo labore electram cum.
-            Iracundia percipitur et usu.
-          </p> */}
+          <img
+            src="./profile_pic.jpeg"
+            height={"100%"}
+            width={"100%"}
+            alt="profile_img"
+          />
         </ListItem>
-        {/* <ListItem style={{ flexGrow: 1 }}>
-          <h3>Who am i ?</h3>
-          <p>
-            Is ut graece delectus repudiare, in habeo labore electram cum.
-            Iracundia percipitur et usu. Nonumes consequat vix et, mea inani
-            veritus democritum ei. His ullum feugait cu, ipsum semper molestiae
-            no ius, eam deleniti. Everti praesent sed an. Animal noluisse usu te
-            eam deleniti Everti.
-          </p>
-        </ListItem> */}
         <ListItem style={{ flexGrow: 3 }}>
-          <h3>Who am i ?</h3>
-          <h2 style={{ textAlign: "center" }}>Vikash Kumar Verma</h2>
-          Front-end Developer
+          <h3>Hello & Welcome UI/UX Designer Photographer Devloper</h3>
+          <h2>I 'M Vikash Verma</h2>
           <p>
-            Is ut graece delectus repudiare, in habeo labore electram cum.
-            Iracundia percipitur et usu. Nonumes consequat vix et, mea inani
-            veritus democritum ei. His ullum feugait cu, ipsum semper molestiae
-            no ius, eam deleniti. Everti praesent sed an. Animal noluisse usu te
-            eam deleniti Everti.
+            Full Stack Developer / Front-end Developer / React - Redux Developer
+          </p>
+          <p>
+            ✓ Adept in end-to-end development of software products from
+            requirement analysis to system study, designing, coding, testing,
+            de-bugging, documentation, and implementation.<br></br> ✓ Deft at
+            mapping the requirements, custom designing solutions &
+            troubleshooting for complex software & application problems.
           </p>
         </ListItem>
       </Container>
